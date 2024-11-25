c8c06fa3cfb28a4fe525a6027901196ffca4d3ec
ScarlR <scarletraunak@gmail.com>
2024-11-25 23:25:24 +0530

    shift to module

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var http = require('http');
+import http from 'http'
 
 http.createServer(function (req, res) {
-    res.write('You are  in AWS  Cloud. This is Nodejs Application, Made with love by Raunak Scarlet \n'); 
-    res.write('\n\n\nApp Version 1.0'); 
-  res.end(); 
+    res.write('You are  in AWS  Cloud. This is Nodejs Application, Made with love by Raunak Scarlet \n');
+    res.write('\n\n\nApp Version 1.0');
+  res.end();
 }).listen(3000);
