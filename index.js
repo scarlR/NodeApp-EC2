e4730ed9df402b1bedb391986d1c147bd022b3e3
ScarlR <scarletraunak@gmail.com>
2024-11-26 00:36:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import http from 'http'
 
 http.createServer(function (req, res) {
     res.write('You are  in AWS  Cloud. This is Nodejs Application, Made with love by Raunak Scarlet \n');
-    res.write('\n\n\nApp Version 1.0');
+    res.write('\n\n\nApp Version 1.0.1');
   res.end();
 }).listen(3000);
